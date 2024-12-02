fcab1a5a1f1ff223271a348139edb8b098f4783c
svcx650 <lpcrisci@gmail.com>
2024-12-02 13:46:58 -0500

    Day 2 made on personal laptop but didn't finish and forgot to commit. Redid it on work laptop, currently works with test input.

---
+++ Day2/Day2fromWork.py
@@ -0,0 +1,44 @@
+
+def IsItSafe(report):
+    levels = [int(i) for i in report.split()]
+    check_inc = []
+    check_dec = []
+    
+    # check if decreasing
+    for index, level in enumerate(levels):
+        if index == 0: continue
+        elif levels[index-1] - level in range(1,4):
+            check_dec.append(True)
+        else:
+            check_dec.append(False)
+
+    # check if increasing
+    for index, level in enumerate(levels):
+        if index == 0: continue
+        elif levels[index-1] - level in range (-3,0):
+            check_inc.append(True)
+        else:
+            check_inc.append(False)
+
+    if all(check_inc) or all(check_dec): return True
+    else: return False
+
+
+# open input
+with open('testinput.txt', 'r') as file:
+    lines = file.read().splitlines()
+
+# format input
+
+
+# find answer
+count = 0
+results = []
+for line in lines:
+    result = IsItSafe(line)
+    results.append(result)
+    if result: count += 1
+
+# report answer
+print(results)
+print(count, ' safe reports')
