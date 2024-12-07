61f54aee52f13ee5a79d2457213216104b7b359f
svcx650 <lpcrisci@gmail.com>
2024-12-06 19:01:54 -0500

    Worked on Day5.

--- Day5/Day5.py
+++ Day5/Day5.py
@@ -1,3 +1,12 @@
+def RuleValidator(update, rules):
+    ####### checks an update for pass/fail/NA to all rules
+    output = []
+    for rule in rules:
+        # check that update contains both pages otherwise NA
+        # check rule against update and P or F
+        output.append()
+    return output # returns list of P/F/NA
+
 # open input
 with open('testinput.txt', 'r') as file:
     lines = file.read().splitlines()
@@ -8,24 +17,34 @@ dividerIndex = 0
 for index, line in enumerate(lines):
     if line == '': 
         dividerIndex = index
-
 rules = []
 for line in lines[:dividerIndex]:
     a, b = line.split('|')
     rule = int(a), int(b)
     rules.append(rule)
-
-update = []
+updates = []
 for line in lines[dividerIndex+1:]:
     page = [int(i) for i in line.split(',')]
-    update.append(page)
+    updates.append(page)
 
-#for each 
+
+
+#for each update find the applicable rules, check them, if all pass then add to correctUpdates
+correctUpdates = []
+for update in updates:
+    p_f_na = RuleValidator(update, rules)# check all rules and determine pass/fail/na
+    if all(result in ('P', 'NA') for result in p_f_na):
+        correctUpdates.append(update)
+
 
 
 # find answer
-
-
+results = [] # will store middle page numbers of correctly ordered updates
+for i in correctUpdates:
+    middleIndex = (len(i) - 1)/2
+    results.append(i[middleIndex])
 
 # # report answer
-# print('Total Count:', countxmases)
+print(sum(results))
+# print(rules)
+# print(updates)
