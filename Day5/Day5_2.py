c11857dcf5cd644e8f87fb5387de7250ec6db97f
svcx650 <160436333+svcx650@users.noreply.github.com.>
2024-12-08 11:02:54 -0500

    finished 5_2.

--- Day5/Day5_2.py
+++ Day5/Day5_2.py
@@ -1,26 +1,68 @@
 def RuleValidator(update, rules):
     ####### checks an update for pass/fail/NA to all rules
-    output = []
+    passed = []
+    failed = []
     for rule in rules:
         # check that update contains both pages otherwise NA
         try:
             a = update.index(rule[0])
             b = update.index(rule[1])
             if a < b:
-                output.append('P')
-            else: output.append('F')
+                passed.append(rule)
+            else: failed.append(rule)
         except ValueError:
-            output.append('NA')
             continue
-    return output
+    return passed, failed
 
 
-# open input
-with open('testinput.txt', 'r') as file:
+
+def Reorder(update, passes, fails):
+    ####### corrects the pages in an update to comply with all applicable rules
+    applicableRules = passes + fails
+    passHistory = [passes]
+    orderedUpdate = update
+    attempt = 0
+    moveY = True
+    while len(fails) > 0:
+        # make changes to update to be more correctly ordered
+        for rule in applicableRules:
+
+            a = orderedUpdate.index(rule[0]) # X and Y are the numbers, a is the index of X and b is index of Y
+            b = orderedUpdate.index(rule[1])
+            if a < b:
+                continue
+            else:
+                if moveY:
+                    # move Y to be true
+                    orderedUpdate.insert(a + 1, rule[1]) # inserts what is now a second instance of Y
+                    orderedUpdate.remove(rule[1]) # removes first instance of Y
+                else: 
+                    # move x to be true
+                    orderedUpdate.insert(b - 1, rule[0]) # inserts what is now a second instance of Y
+                    orderedUpdate.remove(rule[0]) # removes first instance of Y
+
+
+        # check changes and repeat if necessary
+        passes, fails = RuleValidator(update, applicableRules)
+        passHistory.append(passes)
+        attempt += 1
+        ## the below if statements didn't end up being necessary, after thinking about it more I think with enough iterations (assuming solvable rules) the above will always work.
+        if attempt > 1000:
+            print('switching to moveX, update',update, 'is currently', orderedUpdate)
+            moveY = False
+        if attempt > 2000:
+            print('max attempts reached, update',update, 'is currently', orderedUpdate)
+            print(passHistory)
+            return None
+    # print(attempt, 'attempts needed to solve update', update)
+    return orderedUpdate
+
+
+
+# open and format input
+with open('input.txt', 'r') as file:
     lines = file.read().splitlines()
 
-
-# format input
 dividerIndex = 0
 for index, line in enumerate(lines):
     if line == '': 
@@ -38,25 +80,24 @@ for line in lines[dividerIndex+1:]:
 
 
 #for each update find the applicable rules, check them, if all pass then add to correctUpdates
-correctUpdates = []
-for update in updates:
-    p_f_na = RuleValidator(update, rules)# check all rules and determine pass/fail/na
-    # print (p_f_na)
-    if all(result in ('P', 'NA') for result in p_f_na):
-        print(update)
-        correctUpdates.append(update)
 
-# print(correctUpdates)
+allUpdates = [] # each element will have 3 lists, update, passed rules, failed rules
+for update in updates:
+    passes, fails = RuleValidator(update, rules)
+    updateInfo = [update, passes, fails]
+    allUpdates.append(updateInfo)
+        
+incorrectUpdates = []
+for info in allUpdates:
+    if len(info[2]) != 0:
+        incorrectUpdates.append(info)
+
 # find answer
 results = [] # will store middle page numbers of correctly ordered updates
-for i in correctUpdates:
-    # print('len', len(i))
-    middleIndex = int((len(i) - 1)/2)
-    # print('middle index', middleIndex)
-    # print(i[middleIndex])
-    results.append(i[middleIndex])
+for i in incorrectUpdates:
+    reorderdUpdate = Reorder(i[0],i[1],i[2])
+    middleIndex = int((len(reorderdUpdate) - 1)/2)
+    results.append(reorderdUpdate[middleIndex])
 
 # # report answer
 print('answer:', sum(results))
-# print(rules)
-# print(updates)
