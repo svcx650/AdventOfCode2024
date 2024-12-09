578b78d3bfc6fb734d98bde64e52835b286bc3de
svcx650 <lpcrisci@gmail.com>
2024-12-09 11:56:13 -0500

    Initialized Day8 and Day9, noticed I forgot to push Day7 from home PC.

---
+++ Day9/Day9.py
@@ -0,0 +1,15 @@
+### open input
+with open('testinput.txt', 'r') as file:
+    lines = file.read().splitlines()
+
+
+### format input
+
+
+
+### find answer
+results = []
+
+
+### report answer
+print('Answer:', sum(results))
