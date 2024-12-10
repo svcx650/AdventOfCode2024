20643a91976485e2fda06961a2dfad4bf5108119
svcx650 <lpcrisci@gmail.com>
2024-12-10 15:45:43 -0500

    Worked on Day10, still incomplete.

--- Day10/Day10.py
+++ Day10/Day10.py
@@ -1,4 +1,23 @@
-def TrailFinder(position, row, column, up, down, left, right)
+def TrailFinder(position, mappedArea):
+    nextSteps = []
+    try:
+        up = (row-1,column)
+        trail.append(up)
+    except IndexError: pass
+    try:
+        down = (row+1,column)
+        trail.append(down)
+    except IndexError: pass
+    try:
+        left = (row,column-1)
+        trail.append(left)
+    except IndexError: pass
+    try:
+        right = (row,column+1)
+        trail.append(right)
+    except IndexError: pass
+    return nextSteps
+
 
 ### open input
 with open('testinput.txt', 'r') as file:
@@ -14,11 +33,26 @@ for line in lines:
 # for each 0, find all trails that end in 9 (incrementing by 1, moving up down left right)
 scores = []
 for row, line in mappedArea:
-    for column, position in line:
+    for column, char in line:
         score = 0
         trail = []
-        if position == 0:
+        if char == 0:
+            position = (row,column)
+        nextSteps = [1]
+        while nextSteps != []
+            nextSteps = []
+            nextSteps = TrailFinder(position, mappedArea)
+            for step in nextSteps:
+                step
+            for location in trail:
+                #check other locations
+
+
+
+
+            while
             # begin checking for trails
+            try: up == 
             # try all directions
             ## for any valid directions that equal position += 1 
             ## store coordinates in trail as tuple if it does not already exist in trail
